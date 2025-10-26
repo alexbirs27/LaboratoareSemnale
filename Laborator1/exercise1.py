ed91eaedd4c2c3d1ad68ba70927c5d117affa1e3
Alex <birsanalexcristian@gmail.com>
2025-10-26 10:52:31 +0200

    minor fix

--- Laborator1/exercise1.py
+++ Laborator1/exercise1.py
@@ -21,9 +21,8 @@ fig.suptitle('Tignale cosinusoidale')
 axs[0].plot(t, x)
 axs[1].plot(t, y)
 axs[2].plot(t, z)
-plt.show()
 plt.savefig('Laborator1/exercitiu1figura1.pdf')
-
+plt.show()
 
 frequency2 = 200 # 200 Hz
 t = np.linspace(startTime, endTime, int(frequency2 * (endTime - startTime)))
@@ -37,5 +36,5 @@ fig.suptitle('Tignale cosinusoidale - frecventa mica ESANTIONAT')
 axs[0].stem(t, x1)
 axs[1].stem(t, y1)
 axs[2].stem(t, z1)
-plt.show()
 plt.savefig('Laborator1/exercitiu1figura2.pdf')
+plt.show()
