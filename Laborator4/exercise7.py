db6168239c4a17338d1a9aa8dcb757f9b6febc2c
Alex,Birsan <birsanalexcristian@gmail.com>
2025-11-05 12:37:39 +0200

    Lab4: ex4 ex7

---
+++ Laborator4/exercise7.py
@@ -0,0 +1,7 @@
+'''
+in logaritm raportul este diferenta so...
+SNR = raport putere semnal si cea a zgomotului
+SNRdB = 10*log10(Psignal/Pzgomot)
+Pzgomotdb = Psemnaldb - SNRdB
+Pzbomotdb = 90-80 = 10dB
+'''
---
+++ Laborator4/exersice4.py
@@ -0,0 +1,4 @@
+'''
+Raspunsul este 400  Hz.
+Pentru ca orice din intervalul 40-200 sa fim safe, trebuie sa luam o frecventa de esantionare de minim 400 Hz (max(40-200)*2=400).
+'''
